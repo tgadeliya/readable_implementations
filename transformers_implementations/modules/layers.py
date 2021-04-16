378b1d5537843f26c32dc79cf334a66f3a3d169e
Timur Gadelia <tgadeliya@gmail.com>
2021-04-16 20:39:26 +0000

    Add basic Encoder and Decoder for Transformer

---
+++ transformers_implementations/modules/layers.py
@@ -0,0 +1,39 @@
+import torch
+from torch.nn import Module
+from ..modules.attention import MultiHeadAttention
+from ..modules.attention import MultiHeadAttention
+from ..modules.utils import TransformerFeedForward, LayerNorm
+
+
+class TransformerEncoder(Module):
+    def __init__(self):
+        
+        self.MultiHeadAttention = MultiHeadAttention()
+        self.LayerNorm1 = LayerNorm()
+        self.FeedForward = TransformerFeedForward() 
+        self.LayerNorm2 = LayerNorm()
+    
+    def forward(self, x):
+        x_mha = self.MultiHeadAttention(x)
+        x = self.LayerNorm1(x + x_mha)
+        x_ff = self.FeedForward(x)
+        x = self.LayerNorm2(x+ x_ff)
+        return x
+
+
+class TransformerDecoder(Module):
+    def __init__(self):
+        self.MaskedMultiHeadAttention = MultiHeadAttention()
+        self.MultiHeadAttention = MultiHeadAttention()
+        self.LayerNorm1 = LayerNorm()
+        self.FeedForward = TransformerFeedForward() 
+        self.LayerNorm2 = LayerNorm()
+    
+    def forward(self, x):
+        x_mmha = self.MaskedMultiHeadAttention(x)
+        x = self.LayerNorm1(x + x_mmha)
+        x_mha = self.MultiHeadAttention(x)
+        x = self.LayerNorm1(x + x_mha)
+        x_ff = self.FeedForward(x)
+        x = self.LayerNorm1(x + x_ff)
+        return x
