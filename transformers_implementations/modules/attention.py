c91d5207e499ebd3b1058c037e8dc58b744690a3
Timur Gadelia <tgadeliya@gmail.com>
2021-04-16 20:39:51 +0000

    Initialize Attention to future implementation

---
+++ transformers_implementations/modules/attention.py
@@ -0,0 +1,11 @@
+import torch
+from torch.nn import Module
+
+
+class MultiHeadAttention(Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
