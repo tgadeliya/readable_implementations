92d2e8ec8ff952dc30ff8c2b4cc950d16e68e815
Timur Gadelia <tgadeliya@gmail.com>
2021-04-24 20:07:48 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+from setuptools import find_packages
 import setuptools
 
 setuptools.setup(
     name="transformers_implementations",
-    version="0.0.0",
+    version="0.1.0",
+    packages = find_packages()
 )
