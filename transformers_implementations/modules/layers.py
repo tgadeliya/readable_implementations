0dfcac9c4c4384ae375e3c6cc4cf8764e2160d82
Timur Gadelia <tgadeliya@gmail.com>
2021-04-24 21:19:31 +0000

    Implement init encoder

--- transformers_implementations/modules/layers.py
+++ transformers_implementations/modules/layers.py
@@ -1,23 +1,27 @@
 import torch
 from torch.nn import Module
 from ..modules.attention import MultiHeadAttention
-from ..modules.attention import MultiHeadAttention
 from ..modules.utils import TransformerFeedForward, LayerNorm
 
 
 class TransformerEncoder(Module):
     def __init__(self):
-
+        super().__init__()
         self.MultiHeadAttention = MultiHeadAttention()
         self.LayerNorm1 = LayerNorm()
         self.FeedForward = TransformerFeedForward()
         self.LayerNorm2 = LayerNorm()
 
     def forward(self, x):
+        print("X: ", x.size())
         x_mha = self.MultiHeadAttention(x)
+        print("X_MHA: ", x.size())
         x = self.LayerNorm1(x + x_mha)
+        print("X_INTER: ", x.size())
         x_ff = self.FeedForward(x)
+        print("X_FF: ", x_ff.size())
         x = self.LayerNorm2(x + x_ff)
+        print("X_FINISH: ", x.size())
         return x
 
 
