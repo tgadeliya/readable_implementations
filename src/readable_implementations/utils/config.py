6fa6a2e6a9371bfb6f48f4a5d1f505a91a113221
Tsimur Hadeliya <tsimur.hadeliya@allegro.pl>
2022-04-05 01:08:40 +0200

    Add missing files to git

---
+++ src/readable_implementations/utils/config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim import Adam, Optimizer
+
+
+@dataclass
+class BasicConfig:
+    device: str
+    optimizer: Optimizer
+    model: nn.Module
+    loss_fn: nn.Module
+    train_dataloader: DataLoader
+    val_dataloader: DataLoader
+    max_epochs: int
+    validate_every_batch: int
