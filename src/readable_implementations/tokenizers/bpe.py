a0b0c871a5191dfe588d9fea102c6a099d4a9584
Tsimur Hadeliya <tsimur.hadeliya@allegro.pl>
2022-05-01 01:53:27 +0200

    Implement basic BPE Trainer

---
+++ src/readable_implementations/tokenizers/bpe.py
@@ -0,0 +1,20 @@
+
+class BPE:
+    def __init__(self, vocab, merges) -> None:
+        self.vocab = 1
+        self.merges = 1
+
+    def __call__(self, *args, **kwargs):
+        """
+        Main tokenize function
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def decode(self):
+        pass
+
+    def encode(self):
+        pass
